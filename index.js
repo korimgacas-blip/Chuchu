be383c01cdc93729d40cb5d225cb942e317e76ec
korimgacas-blip <korimgacas@gmail.com>
2026-02-14 07:48:04 -0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    // Esta linha mágica diz ao Worker para entregar seus arquivos estáticos (HTML/CSS)
-    // que estão configurados na opção 'assets' do wrangler.jsonc
-    return env.ASSETS.fetch(request);
-  },
-};
